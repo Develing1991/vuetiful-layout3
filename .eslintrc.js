a0ca1bae7d6a7f3649e4565b2d277ad01de13ae2
suhan <completed0728@gmail.com>
2022-07-15 17:11:48 +0900

    sdf

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,37 @@ module.exports = {
     parser: "babel-eslint",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    //셀프클로징 추가 //
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always', //빈 태그 항상 /셀프클로징
+          normal: 'never', //일반태그는 제외
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    //'no-console': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    //'prettier/prettier': ['로그레벨', {프리티어 속성}]
+    'prettier/prettier': [
+      'error', //'off' or 'warn' or 'error' 로그레벨 정의
+      {
+        singleQuote: true, //  "" -> ''
+        semi: true, // alert('1') -> alert('1');
+        useTabs: true, //공백 대신 탭을 사용해서 들여 씀
+        tabWidth: 2, //탭 간격( 공백 수2 )
+        printWidth: 80, //80칸이 넘어가면 개행
+        bracketSpacing: true, //{foo: bar} -> { foo: bar }
+        arrowParens: 'avoid', // always : (x) => x, avoid : x => x
+        trailingComma: 'all', //객체에서 후행 쉼표 사용
+        endOfLine: 'auto', // 줄끝 라인 맞추기
+        vueIndentScriptAndStyle: true,// 스타일, 스크립트 인덴팅 자동정렬
+      },
+    ],
   },
 };
--- src/layouts/header/PopAppbar.vue
+++ src/layouts/header/PopAppbar.vue
@@ -2,7 +2,7 @@
 	<v-app-bar app color="white elevation-1">
 		<div class="font-weight-bold">서비스 이용 약관</div>
 		<v-spacer />
-
+		<!-- dfds -->
 		<div @click="$emit('right')">
 			<v-icon>mdi-close</v-icon>
 		</div>
