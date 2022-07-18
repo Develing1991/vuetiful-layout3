1e811f3250020c6aeb569bc073374bdc3b766fba
suhan <completed0728@gmail.com>
2022-07-18 12:21:18 +0900

    라우터

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,5 +1,12 @@
-/* eslint-disable */
+///* eslint-disable */
 import Vue from 'vue';
+
+// const isMobile = () => {
+// 	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+// 		navigator.userAgent,
+// 	);
+// };
+
 export default function createComponent(...childs) {
 	const DefaultComponent = Vue.component('TempView', {
 		template: '<div></div>',
@@ -8,13 +15,25 @@ export default function createComponent(...childs) {
 		data: function () {
 			return {
 				message: '메시지테스트',
+				maxWidth: '720px',
+				margin: '0 auto',
 			};
+		},
+		mixin: [''],
+		computed: {
+			isMobile() {
+				const isMobile =
+					/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+						navigator.userAgent,
+					);
+				return !isMobile ? '720px' : '';
+			},
 		},
 		template: `
     <div>
-      <CustomAppbar @left="clickLeft" @center="clickCenter" @right="clickRight"></CustomAppbar>
-      <CustomView></CustomView>
-      <CustomFooter></CustomFooter>
+      <CustomAppbar @menu="menu"  @left="clickLeft" @center="clickCenter" @right="clickRight" :style="{maxWidth:maxWidth, margin:margin}"></CustomAppbar>
+      <CustomView :style="{maxWidth:maxWidth, margin:margin}"></CustomView>
+      <CustomFooter :style="{maxWidth:maxWidth, margin:margin}"></CustomFooter>
     </div>
     `,
 		components: {
@@ -22,7 +41,6 @@ export default function createComponent(...childs) {
 			CustomView: childs[1] ?? DefaultComponent,
 			CustomFooter: childs[2] ?? DefaultComponent,
 		},
-		computed: {},
 		mounted() {
 			console.log('index.vue생성');
 		},
